4619d1e7954b93fe622a8a00c8c3705c518a41f3
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2023-05-10 13:14:34 +0700

    add final project init

---
+++ src/chat/Client.java
@@ -0,0 +1,10 @@
+package chat;
+
+public class Client {
+
+    public static void main(String[] args) {
+        chat_client client;
+        client = new chat_client("127.0.0.1");
+        client.startRunning();
+    }
+}
