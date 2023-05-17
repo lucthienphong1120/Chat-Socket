423c1ba4157f2071c62bb9aba9c3d03cbcb9b4e0
Your Name <you@example.com>
2023-05-17 10:19:42 +0700

    fix

--- casestudy1/model/LoginModel.java
+++ casestudy1/model/LoginModel.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package casestudy1.model;
-
-/**
- *
- * @author Phong
- */
 public class LoginModel {
 
     private String username;
