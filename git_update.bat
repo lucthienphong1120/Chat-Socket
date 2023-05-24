b94b2228c1121169540945e67749c7bfc0ed85dd
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2023-05-24 14:46:44 +0700

    test script

--- git_update.bat
+++ git_update.bat
@@ -2,26 +2,27 @@
 color 2
 
 REM Step 1: Git pull
-echo Pull the lastest news from Github...
+echo [!] Pull the lastest news from Github...
 git pull
 
 REM Check if git pull encountered an error
 if errorlevel 1 (
-    echo Git pull failed. Please resolve conflicts manually.
+    color 4
+    echo [!] Git pull failed. Please resolve conflicts manually.
     pause
     exit
 )
 
 REM Step 2: Git add -A
-echo Add current change to git
+echo [!] Add current change to git
 git add -A
 
 REM Step 3: User input for commit message
-set /p message="Enter commit message: "
+set /p message="[!] Enter commit message: "
 git commit -m "%message%"
 
 REM Step 4: Git push
-echo Push all changes to Github...
+echo [!] Push all changes to Github...
 git push
 
 REM End of script
