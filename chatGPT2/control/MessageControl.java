ec97d71a42bc7f545abdac665925aef9ba1f37bd
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2023-05-25 23:57:23 +0700

    update and save file, data.json

--- chatGPT2/control/MessageControl.java
+++ chatGPT2/control/MessageControl.java
@@ -1,7 +1,8 @@
 package chatGPT2.control;
 
 import chatGPT2.model.*;
-import org.json.simple.JSONArray;
+import java.io.BufferedReader;
+import java.io.File;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -19,54 +20,69 @@ public class MessageControl {
 
     public MessageControl(String fileName) {
         this.fileName = fileName;
+        createFile();
+        resetFile();
+        // Đăng ký sự kiện xoá file khi đóng chương trình
+//        Runtime.getRuntime().addShutdownHook(new Thread(this::deleteFile));
     }
 
     public void saveMessage(MessageModel message) {
-        JSONArray jsonArray = loadMessages();
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("id", generateID());
         jsonObject.put("name", message.getName());
         jsonObject.put("message", message.getMessage());
         jsonObject.put("time", message.getTime());
-        jsonArray.add(jsonObject);
 
-        try (FileWriter fileWriter = new FileWriter(fileName)) {
-            fileWriter.write(jsonArray.toJSONString());
-            System.out.println("Đã lưu tin nhắn vào file thành công.");
+        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+            fileWriter.write(jsonObject.toJSONString() + "\n");
         } catch (IOException ex) {
             Logger.getLogger(MessageControl.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     public List<MessageModel> loadAllMessages() {
-        JSONArray jsonArray = loadMessages();
-        List<MessageModel> messages = new ArrayList<>();
-
-        for (Object obj : jsonArray) {
-            JSONObject jsonObject = (JSONObject) obj;
-            String name = (String) jsonObject.get("name");
-            String message = (String) jsonObject.get("message");
-            String time = (String) jsonObject.get("time");
-            messages.add(new MessageModel(name, message, time));
-        }
-
-        return messages;
-    }
-
-    private JSONArray loadMessages() {
-        JSONArray jsonArray = new JSONArray();
-
-        try (FileReader fileReader = new FileReader(fileName)) {
+        List<MessageModel> listMessage = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
             JSONParser jsonParser = new JSONParser();
-            Object obj = jsonParser.parse(fileReader);
-            if (obj != null) {
-                jsonArray = (JSONArray) obj;
+            while ((line = reader.readLine()) != null) {
+                JSONObject jsonObject = (JSONObject) jsonParser.parse(line);
+                String name = (String) jsonObject.get("name");
+                String message = (String) jsonObject.get("message");
+                String time = (String) jsonObject.get("time");
+                listMessage.add(new MessageModel(name, message, time));
             }
         } catch (IOException | ParseException ex) {
             Logger.getLogger(MessageControl.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return listMessage;
+    }
+    
+    private void createFile() {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                Logger.getLogger(MessageControl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 
-        return jsonArray;
+    private void resetFile() {
+        try (FileWriter fileWriter = new FileWriter(fileName, false)) {
+            // Ghi một dòng trống vào file để xoá hết dữ liệu cũ
+            fileWriter.write("");
+        } catch (IOException ex) {
+            Logger.getLogger(MessageControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void deleteFile() {
+        File file = new File(fileName);
+        if (file.exists()) {
+            file.delete();
+        }
     }
 
     private String generateID() {
---
+++ chatGPT2/control/NewClass.java
@@ -0,0 +1,24 @@
+package chatGPT2.control;
+
+import chatGPT2.model.MessageModel;
+import java.util.List;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        MessageControl messageControl = new MessageControl("./src/data.json");
+        // write file
+        messageControl.saveMessage(new MessageModel("test 1", "test", "test"));
+        messageControl.saveMessage(new MessageModel("test 2", "test", "test"));
+        messageControl.saveMessage(new MessageModel("test 3", "test", "test"));
+        messageControl.saveMessage(new MessageModel("test 4", "test", "test"));
+//        // read file
+//        List<MessageModel> listMessage = messageControl.loadAllMessages();
+//        System.out.println(listMessage.size());
+//        for (MessageModel m : listMessage) {
+//            System.out.println("...");
+//            System.out.println(m.getName() + " " + m.getMessage());
+//        }
+        
+    }
+}
